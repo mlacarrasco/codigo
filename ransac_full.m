19ff248cb72b04561e0098f2ea3646db7037a0ed
mlacarrasco <mlacarrasco@gmail.com>
2017-09-06 18:28:23 -0300

    v.0.02

--- ransac_full.m
+++ ransac_full.m
@@ -2,14 +2,15 @@
 % Proyecto Elecmetal
 % Miguel A. Carrasco. (mlacarrasco@gmail.com)
 % v0.01. 30-08-2017  %buscamos una combinación con el menor error)
-% v0.02. 06-09-2017  %modificacion codigo
+% v0.02. 06-09-2017  %busqueda de polinomio que intersecta
 
-function [SEL, indices, thickness]=ransac_full(pts, id_cont, id_sel, pant)
+function [SEL, indices,POL_SEL]=ransac_full(pts, id_cont, id_sel, POL, pant)
 
 SEL      = [];
 best_cmb = [];
 indices  = [];
-backup= pts;
+backup   = pts;
+POL_SEL  = [];
 
 %id_sel: indices de puntos correlacionados
 pts       = pts(:,id_sel);
@@ -25,29 +26,53 @@ if (size(pts,2)>=4)
     backup_ind= indices;
     pts= pts_s;
 end
-err = zeros(size(cmb,1),1);
+err   = zeros(size(cmb,1),1);
+pol_s = zeros(size(cmb,1),1);
 
 for i=1:size(cmb,1)
     
     cols= cmb(i,:);
-    SEL = pts(:, cols);
-    P= [polyfit(SEL(1,:), SEL(2,:), 1) 1];
+    %coordenadas seleccionadas (combinatorial)
+    SEL = pts(:, cols); 
     
-    %ploteamos una recta que intersecta los puntos agrupados en el
-    %subcluster.
-    if (pant)
-        ax= axis(); xa= ax(1):ax(2); yfit=P(1)*xa'+P(2);
-        hold on;plot(xa, yfit,'y-.');drawnow;
+    %P= [polyfit(SEL(1,:), SEL(2,:), 1) 1];
+    tmp=[];
+    %para cada punto
+    for j=1:size(SEL,2)
+        
+        pt=[SEL(:,j)' 1];
+        G=[];
+        
+        %calculamos la distancia del punto a todas las rectas.
+        for k=1:size(POL,1)
+            G(k) = (abs(POL(k,1)*pt(1) - 1*pt(2) + POL(k,2))) / (sqrt(POL(k,1)^2 + 1));
+        end
+        
+        %buscamos la mejor recta.
+        [val, id]=min(G);
+        tmp = [tmp; val id];
+        
     end
+    s= sum(tmp,1);
+    err(i) = s(1);
+    pol_s(i) = mode(tmp(:,2));
     
-    distance = zeros(size(pts,2),1);
-    for p=1:size(pts,2)
-        %calculamos la distancia de un punto a la recta (polinomio)
-        %forma del polinomio y= -a/b*x - c/b;
-        distance(p) = (abs(-P(1)*pts(1,p) + 1*pts(2,p) - P(3))) / (sqrt(P(1)^2 + P(2)^2));
-        %plot(SEL(1,p),SEL(2,p),'ys','MarkerSize',10);
-    end
-    err(i) = mean(distance);
+    
+%     %ploteamos una recta que intersecta los puntos agrupados en el
+%     %subcluster.
+%     if (pant)
+%         ax= axis(); xa= ax(1):ax(2); yfit=P(1)*xa'+P(2);
+%         hold on;plot(xa, yfit,'y-.');drawnow;
+%     end
+%     
+%     distance = zeros(size(pts,2),1);
+%     for p=1:size(pts,2)
+%         %calculamos la distancia de un punto a la recta (polinomio)
+%         %forma del polinomio y= -a/b*x - c/b;
+%         distance(p) = (abs(-P(1)*pts(1,p) + 1*pts(2,p) - P(3))) / (sqrt(P(1)^2 + P(2)^2));
+%         %plot(SEL(1,p),SEL(2,p),'ys','MarkerSize',10);
+%     end
+%     err(i) = mean(distance);
     
 end
 
@@ -57,6 +82,7 @@ best_cmb = cmb(id_sort(1),:)
 SEL= pts(:,best_cmb);
 indices = indices(best_cmb);
 thickness = size(SEL,2);
+POL_SEL = POL(pol_s(id_sort(1)),:);
 catch
     SEL=[];
     best_cmb=[];
